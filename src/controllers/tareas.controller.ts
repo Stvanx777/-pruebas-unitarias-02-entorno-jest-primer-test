8b87ad70b88fdbde7d44c174c43eb0ca4b1b2c30
Esteban Antonio Serrano Morales <angusxd123@Mac-mini-de-Esteban.local>
2026-04-21 22:47:48 -0600

    todo completado

--- src/controllers/tareas.controller.ts
+++ src/controllers/tareas.controller.ts
@@ -1,31 +1,23 @@
-// Necesitamos importar estos tipos de Express para poder trabajar con las peticiones HTTP
-// Request: contiene toda la información que nos envía el cliente (como datos del formulario, parámetros, etc.)
-// Response: nos permite enviar respuestas al cliente (como datos, mensajes de error, etc.)
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import { obtenerTodasLasTareas, crearTarea } from "../services/tareas.service";
 
-// Por ahora, en lugar de usar una base de datos, guardamos las tareas en un arreglo
-// Tenemos un arreglo de objectos en donde cada objeto representa una tarea
-const tareas = [
-  { 
-    id: 1, 
-    titulo: "Estudiar pruebas", 
-    completada: false, 
-    descripcion: "Estudiar pruebas unitarias usando Jest" 
-  },
-  { 
-    id: 2, 
-    titulo: "Hacer ejercicio", 
-    completada: true, 
-    descripcion: "Correr 30 minutos a 10km/h" 
-  },
-];
+export function getTareas(req: Request, res: Response): void {
+  res.json(obtenerTodasLasTareas());
+}
 
-// Esta función se ejecuta cuando alguien hace una petición GET a nuestra API
-// Por ejemplo, cuando un navegador o una app móvil quiere ver la lista de tareas
-// en el archivo app.ts vamos a usar esta función para obtener las tareas
-export function obtenerTareas(req: Request, res: Response) {
-  // Con res.json() convertimos nuestro arreglo de tareas a formato JSON
-  // y lo enviamos como respuesta al cliente
-  // Es como decir: "aquí tienes la lista de tareas que pediste"
-  res.json(tareas);
+export function postTarea(req: Request, res: Response): void {
+  const { titulo, descripcion } = req.body;
+
+  if (!titulo || !descripcion) {
+    res.status(400).json({ mensaje: "Faltan campos obligatorios" });
+    return;
+  }
+
+  const nueva = crearTarea({
+    titulo,
+    descripcion,
+    completada: false,
+  });
+
+  res.status(201).json(nueva);
 }
